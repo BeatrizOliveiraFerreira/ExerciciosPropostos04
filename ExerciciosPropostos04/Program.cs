dc63b0e48db332e10e820d7ac095ff48286c4ee3
Beatriz Oliveira Ferreira <beaferreira.trabalho@gmail.com>
2023-11-06 15:24:46 -0300

    Add project files.

---
+++ ExerciciosPropostos04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            int numeroDeUmFuncionario = int.Parse(Console.ReadLine());
+            int numeroDeHorasTrabalhadas = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double valorQueRecebePorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double calculaSalario = numeroDeHorasTrabalhadas * valorQueRecebePorHora;
+
+            Console.WriteLine($"NUMBER = {numeroDeUmFuncionario}");
+            Console.WriteLine("SALARY = U$ " + calculaSalario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
